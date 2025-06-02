04397dc931843b44725f122096fbdf020fc1ad78
sizzoh <greysonpeter37@gmail.com>
2025-06-02 19:24:56 +0300

    adding build file

--- shoppper/settings.py
+++ shoppper/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vs!wi$b(u6c1cul1!0@p8o_u%oj)z#0b_w66-toj75l@@&frv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
@@ -74,8 +76,14 @@ WSGI_APPLICATION = 'shoppper.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
+if not DEBUG:
+    DATABASES = {
+        "default":{
+            dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
